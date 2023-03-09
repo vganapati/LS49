bd074d8f068395a1a0ec420fb5e1a748224fb44b
vganapati <vganapa1@swarthmore.edu>
2023-03-09 14:14:39 -0800

    Update index1_922530.sh

--- LY99/saul/index1_922530.sh
+++ LY99/saul/index1_922530.sh
@@ -9,7 +9,7 @@
 #SBATCH -o job%j.out
 #SBATCH -e job%j.err
 
-export WORK=$SCRATCH/adse13_249/LY99
+export WORK=$SCRATCH/cytochrome_sim/LY99
 cd $WORK
 
 mkdir $SLURM_JOB_ID; cd $SLURM_JOB_ID
@@ -17,7 +17,7 @@ echo "jobstart $(date)";pwd
 #srun -n 32 -c 2 libtbx.python $MODULES/LS49/adse13_196/test_mpi.py
 # one hour, 10 nodes should suffice
 # NO PSF:
-srun -n 320 -c 2 dials.stills_process ../index1.phil input.glob=../917302/LY99_MPIbatch_*.img.gz
+srun -n 320 -c 2 dials.stills_process $MODULES/LS49/LY99/saul/index1.phil input.glob=../5975501/LY99_MPIbatch_*.img.gz
 
 # WITH PSF:
 #srun -n 320 -c 2 dials.stills_process ../index1.phil input.glob=../917162/LY99_MPIbatch_*.img.gz
