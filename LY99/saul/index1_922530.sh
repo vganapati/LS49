9e9b63826b1287da6d8fcb9cf3fb41507c75407d
vganapati <vganapa1@swarthmore.edu>
2023-03-22 16:02:53 -0700

    Update index1_922530.sh

--- LY99/saul/index1_922530.sh
+++ LY99/saul/index1_922530.sh
@@ -5,7 +5,7 @@
 #SBATCH -A lcls_g          # allocation
 #SBATCH -C gpu
 #SBATCH -q regular    # regular queue
-#SBATCH -t 01:00:00         # wall clock time limit
+#SBATCH -t 00:10:00         # wall clock time limit
 #SBATCH -o job%j.out
 #SBATCH -e job%j.err
 
@@ -17,7 +17,7 @@ echo "jobstart $(date)";pwd
 #srun -n 32 -c 2 libtbx.python $MODULES/LS49/adse13_196/test_mpi.py
 # one hour, 10 nodes should suffice
 # NO PSF:
-srun -n 320 -c 2 dials.stills_process $MODULES/LS49/LY99/saul/index1.phil input.glob=../../5975501/LY99_MPIbatch_*.img.gz
+srun -n 320 -c 2 dials.stills_process $MODULES/LS49/LY99/saul/index1.phil input.glob=../../6316798/LY99_MPIbatch_*.img.gz
 
 # WITH PSF:
 #srun -n 320 -c 2 dials.stills_process ../index1.phil input.glob=../917162/LY99_MPIbatch_*.img.gz
